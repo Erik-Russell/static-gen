1b6142f3c24144836b40e67646f75a1025a36f3a
Erik-Russell <erik.z.russell@gmail.com>
2025-03-16 09:50:50 -0700

    HTMLNode and dummy data in main

---
+++ src/htmlnode.py
@@ -0,0 +1,39 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+
+        self.tag = tag
+        """
+        string for HTML tag name (e.g. "p", "a", "h1")
+        """
+
+        self.value = value
+        """
+        string for value of HTML tag (e.g. text inside a p)
+        """
+
+        self.children = children
+        """
+        list of HTMLNode obj that are the children of this node
+        """
+
+        self.props = props
+        """
+        Dictionary of key-value pairs representing the attributes of the HTML tag.
+        For example, a link (<a> tag) might have {"href": "https://www.google.com"}
+        """
+
+    def __repr__(self):
+        props_str = self.props_to_html() if self.props else ''
+        children_str = ', '.join(repr(child) for child in self.children) if self.children else ''
+
+        return f"HTMLNode(tag='{self.tag}', value='{self.value}', children=[{children_str}], props='{props_str}')"
+
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        output = ''
+        for k, v in self.props.items():
+            output += f' {k}="{v}"'
+        return output
--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from textnode import TextType, TextNode
+from htmlnode import HTMLNode
 
 def main():
     node = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
     print(node)
+
+    p_node = HTMLNode(tag='p', value='This is a paragraph.', props={'class': 'text'})
+    a_node = HTMLNode(tag='a', value='Google', props={'href': 'https://www.google.com'})
+    div_node = HTMLNode(tag='div', children=[p_node, a_node])
+    print(div_node)
 
 if __name__ == '__main__':
     main()
